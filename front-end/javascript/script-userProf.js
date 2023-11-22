25f11e32e868cf6f8b3740239d03a57e6dfd8003
gabrielratao <gabiratao28@gmail.com>
2023-11-22 20:50:33 -0300

    new Read crud

--- front-end/javascript/script-userProf.js
+++ front-end/javascript/script-userProf.js
@@ -37,8 +37,18 @@
 
 // Mostrar dados do usuário
 
-function readUser(){
-    
+async function readUser(){
+    var email = document.getElementById('email').value
+
+    const response = await fetch(`http://localhost:3000/users/${email}`, {
+        method: "GET",
+        dataType: "json"
+    });
+
+    const result = await response.json()
+
+    // if (result === '')
+    console.log(result.recordset)
 }
 
 // function readUser(){
@@ -56,31 +66,31 @@ function readUser(){
 
 // Alterar dados do usuario
 
-const { text } = require("body-parser");
+
 
 
 //---------------------------------------------------------------
 // Deletar usuario
-document.addEventListener("DOMContentLoaded", function () {
+// document.addEventListener("DOMContentLoaded", function () {
 
-    const btnCall = document.getElementById("DelDados");
+//     const btnCall = document.getElementById("DelDados");
     
-    btnCall.addEventListener('click', function() {
+//     btnCall.addEventListener('click', function() {
         
 
-        let senhaConfirm = prompt ("Digite sua senha para confirmar");
-        if (senhaConfirm == "senha correta") {
-            //Aqui entra a logica do delete que foi feito no back-en
+//         let senhaConfirm = prompt ("Digite sua senha para confirmar");
+//         if (senhaConfirm == "senha correta") {
+//             //Aqui entra a logica do delete que foi feito no back-en
 
-            //redireciona para a pagina de despedida
-            window.location.href = "despedida.html";
-        } else {
-            alert("Senha incorrenta. Por favor, tente novamente.")
-        }
-    })
+//             //redireciona para a pagina de despedida
+//             window.location.href = "despedida.html";
+//         } else {
+//             alert("Senha incorrenta. Por favor, tente novamente.")
+//         }
+//     })
 
 
-})
+// })
 
 /*function deletConfirm () {
     let alert;
