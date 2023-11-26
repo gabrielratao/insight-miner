2e7c8538cd9838284224dd7d78d0a06ce3681f6e
gabrielratao <gabiratao28@gmail.com>
2023-11-26 15:17:58 -0300

    adding error exception on app.js

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -63,7 +63,7 @@ app.get('/users_dep/:email', (req, res) =>{
         execSQLQuery(`exec sp_Mostrar_Geral @email = '${email}' `, res)
     }
 	catch (error){
-
+        console.error('Erro no endpoint /users_dep/:email', error.message);
     }
     
 
@@ -73,52 +73,82 @@ app.get('/users_dep/:email', (req, res) =>{
 //Read users by email
 app.get('/users/:email', (req, res) =>{
 
-    const email = req.params.email
-    execSQLQuery(`exec sp_Mostrar_Usuario_Dono @email = '${email}' `, res)
+    try{    
+        const email = req.params.email
+        execSQLQuery(`exec sp_Mostrar_Usuario_Dono @email = '${email}' `, res)
+    }
+    catch (error){
+        console.error('/users/:email', error.message);
+    }
+    
 
 })
 
 //Read tipo de plano
 app.get('/users/plano/:email', (req, res) => {
-    const email = req.params.email
-    execSQLQuery(`exec sp_Mostrar_Plano @email = '${email}' `, res)
+    try{
+        const email = req.params.email
+        execSQLQuery(`exec sp_Mostrar_Plano @email = '${email}' `, res)
+    }
+    catch (error){
+        console.error('/users/plano/:email', error.message);
+    }
+    
 })
 
 //autenticação do usuário
 app.get('/users/autenticate/:email/:senha', (req, res) =>{
-    const email = req.params.email
-    const senha_enviada = req.params.senha
 
-    execSQLQuery(`exec sp_Autenticar_Usuario_Dono
-                @email = '${email}',
-                @senha_enviada = '${senha_enviada}'`, res)
+    try{
+        const email = req.params.email
+        const senha_enviada = req.params.senha
+
+        execSQLQuery(`exec sp_Autenticar_Usuario_Dono
+                    @email = '${email}',
+                    @senha_enviada = '${senha_enviada}'`, res)
+    }
+    catch (error){
+        console.error('/users/autenticate/:email/:senha', error.message);
+    }
 })
 
 
 //create user  Infos required: email, @‌nome @‌email @‌dt_nascimento @‌senha @‌nome_empresa @‌tipo_empresa
 app.post('/users', (req, res) => {
-    const email = req.body.email
-    const nome = req.body.nome
-    const dt_nascimento = req.body.dt_nascimento
-    const senha = req.body.senha
-    const nome_empresa = req.body.nome_empresa
-    const tipo_empresa = req.body.tipo_empresa
 
-    execSQLQuery(`exec sp_Criar_Juridico_Dono 
-                @email = '${email}',
-                @nome = '${nome}',
-                @dt_nascimento = '${dt_nascimento}',
-                @senha = '${senha}',
-                @nome_empresa = '${nome_empresa}',
-                @tipo_empresa = '${tipo_empresa}'`, res)
+    try{
+        const email = req.body.email
+        const nome = req.body.nome
+        const dt_nascimento = req.body.dt_nascimento
+        const senha = req.body.senha
+        const nome_empresa = req.body.nome_empresa
+        const tipo_empresa = req.body.tipo_empresa
+    
+        execSQLQuery(`exec sp_Criar_Juridico_Dono 
+                    @email = '${email}',
+                    @nome = '${nome}',
+                    @dt_nascimento = '${dt_nascimento}',
+                    @senha = '${senha}',
+                    @nome_empresa = '${nome_empresa}',
+                    @tipo_empresa = '${tipo_empresa}'`, res)
+    }
+    catch (error){
+        console.error('/users', error.message);
+    }
+
 })
 
 
 //delete user by email
 app.delete('/users/:email', (req, res) => {
-    const email = req.params.email
 
-    execSQLQuery(`exec sp_Deletar_Usuario_Dono @email = '${email}'`, res)
+    try{
+        const email = req.params.email
+        execSQLQuery(`exec sp_Deletar_Usuario_Dono @email = '${email}'`, res)
+    }
+    catch (error){
+        console.error('/users/:email', error.message);
+    }  
 
 })
 
